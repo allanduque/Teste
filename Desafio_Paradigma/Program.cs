45a312169835d3c64300bfdcdc6b6b4266be78d3
Allan <allanduque.21@gmail.com>
2021-10-25 20:21:55 -0300

    Init

---
+++ Desafio_Paradigma/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Desafio_Paradigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int[] tree = { 3,2,1,6,0,5 };
+            int[] tree = { 7,5,13,9,1,6,4 };
+            string result = Tree.GerarArvore(tree);
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Desafio_Paradigma/Tree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio_Paradigma
+{
+    public class Tree
+    {
+        internal static string GerarArvore(int[] entrada)
+        {
+            int maiorValor = 0;
+            ArrayList galhoEsq = new ArrayList();
+            ArrayList galhoDir = new ArrayList();
+            //localiza o nó
+            foreach (int item in entrada)
+            {
+                if (item > maiorValor) maiorValor = item;
+            }
+
+            if (entrada[0] != maiorValor)
+            {
+                for (int i = 0; entrada[i] != maiorValor; i++)
+                {
+                    galhoEsq.Add(entrada[i]);
+                }
+            }
+
+            if (entrada[entrada.Length-1] != maiorValor)
+            {
+                for (int i = entrada.Length-1; entrada[i] != maiorValor; i--)
+                {
+                    galhoDir.Add(entrada[i]);
+                }
+            }
+
+            galhoEsq.Sort();
+            galhoDir.Sort();
+            galhoEsq.Reverse();
+            galhoDir.Reverse();
+            string resp = "Array de entrada\n" +
+                "Raiz: " + maiorValor +
+                "\n Galhos da esquerda: "+ PrintGalho(galhoEsq) +
+                "\n Galhos da direita: " + PrintGalho(galhoDir);
+            return resp;
+        }
+
+        private static string PrintGalho(ArrayList galho)
+        {
+            string resp = string.Empty;
+
+            foreach (int item in galho)
+            {
+                resp += item + ", ";
+            }
+            return resp.Substring(0, resp.Length - 2);
+        }
+    }
+}
